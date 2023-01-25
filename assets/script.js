b8578423dc996eba2cfaa4944673cae019fceba9
Jocelyn <115833657+jjocelynn@users.noreply.github.com>
2023-01-25 15:55:07 -0500

    linked bootstrap, days, jQuery, jQuery UI and header displays current date properly

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,6 @@
+let date = dayjs().unix();
+$("#currentDay").text(dayjs.unix(date).format("dddd, MMMM DD"));
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
