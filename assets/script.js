60c8287ecf66f067a0c9823d5198c295dbca7803
Jocelyn <115833657+jjocelynn@users.noreply.github.com>
2023-01-25 20:27:11 -0500

    duplicated section node and printed 12hr time in each spot

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,68 @@
-let date = dayjs().unix();
-$("#currentDay").text(dayjs.unix(date).format("dddd, MMMM DD"));
-
+let unixCode = dayjs().unix();
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  $(".saveBtn").click(function () {
+    console.log("ello mate");
+  })
+
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+
+
+
+
+
+
+  //figure out how to time difference
+
+  //if hour= time make red
+
+  //if hour > time, make green
+
+  //if hour < time, make grey
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+
+  console.log($("section"));
+
+
+  duplicateRows(); //calling the function that makes all the rows
+
+
   // TODO: Add code to display the current date in the header of the page.
+
+  $("#currentDay").text(dayjs().format("dddd, MMMM DD"));
 });
+
+
+
+/////////////function copying and pasting section ///////////
+// https://www.w3schools.com/jsref/met_node_clonenode.asp
+
+let duplicateRows = function () {
+  for (let i = 0; i < 8; i++) {
+    $("#color").clone().appendTo("main"); //copying whole section with id of color and pasting it 8 times into the main body
+    $("#color").attr("id", i); //giving all sections an id number of i
+    let scheduleTime = 9 + i; //setting the work day from 9am
+    let time = document.getElementById(i).children[0];
+    time.textContent = (dayjs().set("hour", scheduleTime).format("ha"));
+  }
+}
+
+//let current2 = dayjs.unix(unixCode).format("h"); //12 hour time
