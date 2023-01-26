aca4d5d2880a33d3b154af724a60e0d4a460206a
Jocelyn <115833657+jjocelynn@users.noreply.github.com>
2023-01-26 13:08:39 -0500

    got the correct colors to show up based on current time but last hour(5pm) does not show up

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,32 @@
 let unixCode = dayjs().unix();
+let i;
+let task = document.querySelector("#task");
+
+let section = document.querySelector("#color");
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  for (i = 9; i < 17; i++) {
 
+
+    let currentTime = dayjs().format("H");
+    console.log(i + "  " + currentTime)
+    if (i == currentTime) {
+      $("#color").addClass("present");
+      console.log(currentTime);
+    } else if (i > currentTime) {
+      $("#color").addClass("future");
+    } else {
+      $("#color").addClass("past");
+    }
+    
+    $("#color").clone().appendTo("main"); //copying whole section with id of color and pasting it 8 times into the main body
+    $("#color").attr("id", i); //giving all sections an id number of i
+    let time = document.getElementById(i).children[0];
+    time.textContent = (dayjs().set("hour", i).format("ha"));
+
+  }
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -11,8 +34,16 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  $(".saveBtn").click(function () {
-    console.log("ello mate");
+  $(".saveBtn").click(function (event) {
+    event.preventDefault();
+
+    console.log("ello mate" + $("section"));
+    localStorage.setItem("task", task.value);
+    localStorage.getItem("task");
+
+    //for whichever section's button is pressed, save values into an object
+
+
   })
 
 
@@ -21,6 +52,11 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+
+
+  // console.log(currentTime);
+  // console.log(JSON.stringify(time));
+  // console.log($("div"));
 
 
 
@@ -39,10 +75,7 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
 
-  console.log($("section"));
-
-
-  duplicateRows(); //calling the function that makes all the rows
+  //calling the function that makes all the rows
 
 
   // TODO: Add code to display the current date in the header of the page.
@@ -55,14 +88,6 @@ $(function () {
 /////////////function copying and pasting section ///////////
 // https://www.w3schools.com/jsref/met_node_clonenode.asp
 
-let duplicateRows = function () {
-  for (let i = 0; i < 8; i++) {
-    $("#color").clone().appendTo("main"); //copying whole section with id of color and pasting it 8 times into the main body
-    $("#color").attr("id", i); //giving all sections an id number of i
-    let scheduleTime = 9 + i; //setting the work day from 9am
-    let time = document.getElementById(i).children[0];
-    time.textContent = (dayjs().set("hour", scheduleTime).format("ha"));
-  }
-}
+
 
 //let current2 = dayjs.unix(unixCode).format("h"); //12 hour time
